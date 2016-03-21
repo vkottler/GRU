a6c17b16fb854fa455043859d503f9360681a4fd
Vaughn Kottler <vkottler@wisc.edu>
2016-03-20 21:48:51 -0500

    Show login form again if credentials did not work.

--- www/signin.php
+++ www/signin.php
@@ -1,4 +1,13 @@
 <?php
+
+function signInForm() {
+	echo '<form method="post" action="">
+           Username: <input type="text" name="user_name" />
+           Password: <input type="password" name="user_pass">
+         <input type="submit" value="Sign in" />
+         </form>';
+}
+
 //signin.php
 include 'connect.php';
 include 'header.php';
@@ -12,16 +21,7 @@ if(isset($_SESSION['signed_in']) && $_SESSION['signed_in'] == true)
 }
 else
 {
-    if($_SERVER['REQUEST_METHOD'] != 'POST')
-    {
-        /*the form hasn't been posted yet, display it
-          note that the action="" will cause the form to post to the same page it is on */
-        echo '<form method="post" action="">
-            Username: <input type="text" name="user_name" />
-            Password: <input type="password" name="user_pass">
-            <input type="submit" value="Sign in" />
-         </form>';
-    }
+    if($_SERVER['REQUEST_METHOD'] != 'POST') signInForm();
     else
     {
         /* so, the form has been posted, we'll process the data in three steps:
@@ -63,8 +63,10 @@ else
                 //the query was successfully executed, there are 2 possibilities
                 //1. the query returned data, the user can be signed in
                 //2. the query returned an empty result set, the credentials were wrong
-                if ($result->num_rows == 0) 
+                if ($result->num_rows == 0) {
                     echo 'You have supplied a wrong user/password combination. Please try again.<br>';
+                    signInForm();
+                }
                 else
                 {
                     //set the $_SESSION['signed_in'] variable to TRUE
