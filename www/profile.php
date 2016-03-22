bb7c3d9a38af259aa925dfb69551428e4cfa10b2
vkottler <vkottler@wisc.edu>
2016-03-22 10:44:13 -0500

    Fixing an issue with the profile page

--- www/profile.php
+++ www/profile.php
@@ -5,6 +5,7 @@ include 'functions.php';
 
 // don't allow people to manipulate URL to edit other people's profiles
 if ($_SESSION['logged_in'] == true && strcmp($_GET['view'],"edit") == 0 && $_GET['id'] != $_SESSION['user_id']) {
+	printf("Your ID: %d ID for page: %d", $_GET['id'], $_SESSION['user_id']);
 	echo 'You can only edit your own profile!';
 }
 
