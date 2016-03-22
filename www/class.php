2b9607e6968a0b24324e53510541ab2cc65d1de8
vkottler <vkottler@wisc.edu>
2016-03-22 09:52:10 -0500

    Some dynamic content on class page.

--- www/class.php
+++ www/class.php
@@ -5,19 +5,33 @@ include 'functions.php';
 
 echo '<h2>Class</h2>';
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	echo var_dump($_POST);
+}
+
+else {
 if (isset($_GET['id'])) {
 	$classData = $conn->query(getClassData($_GET['id']));
 	if ($classData->num_rows != 1) echo 'Class not found.';
 	else {
 		$data = $classData->fetch_row();
 		echo '<h3>'.$data[1].'</h3><br>';
-		$jsArg = "'classDescription'";
-		echo $data[2].'<input type="button" value="Edit Description" onClick="javascript:revealForm('.$jsArg.')" style="display:inline-block;">';
-		echo '<div id="classDescription" style="display:none">Hello!</div>';
+		
+		$jsArg1 = "'classDescription'";
+		$jsArg2 = "'currDescription'";
+		
+		$classForm = '<form action="" method="post">
+		              <textarea name="">'.$data[2].'</textarea><br>
+		              <input type="submit" value="Submit">
+		              </form>';
+		
+		echo '<div id="currDescription">'.$data[2].'<input type="button" value="Edit Description" onClick="javascript:revealForm('.$jsArg1.');hideForm('.$jsArg2.')" style="display:inline-block;"></div>';
+		echo '<div id="classDescription" style="display:none">'.$classForm.'</div>';
 		echo '<br><br>Added by: '.userFullNameFromID($data[4]);
 	}
 }
 else echo 'No Class specified.';
+}
 
 include 'footer.php';
 ?>
--- www/scripts.php
+++ www/scripts.php
@@ -1,8 +1,7 @@
 
 <script type="text/javascript">
 
-function revealForm(divName) {
-	document.getElementById(divName).style.display = "block";
-}
+function revealForm(divName) { document.getElementById(divName).style.display = "block"; }
+function hideForm(divName) { document.getElementById(divName).style.display = "none"; }
 
 </script>
