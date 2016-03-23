56f86fa198131a6dd352723e45f60bacaea9b62b
csgreen3 <csgreen3@wisc.edu>
2016-03-22 20:40:58 -0500

    removed return lines

--- www/profile.php
+++ www/profile.php
@@ -2,9 +2,11 @@
 include 'connect.php';
 include 'header.php';
 include 'functions.php';
+
 $sql = 'SELECT * FROM `forumData`.`users` WHERE user_id='.$_GET['id'];
 $result = $conn->query($sql);
 $data = $result->fetch_row();
+
 // don't allow people to manipulate URL to edit other people's profiles
 if ($_SESSION['signed_in'] == true && strcmp($_GET['view'],"edit") == 0 && $_GET['id'] != $_SESSION['user_id']) {
 	echo 'You can only edit your own profile!';
@@ -13,8 +15,6 @@ if ($_SESSION['signed_in'] == true && strcmp($_GET['view'],"edit") == 0 && $_GET
 // editting your own profile
 else if ($_SESSION['signed_in'] == true && strcmp($_GET['view'],"edit") == 0 && $_GET['id'] == $_SESSION['user_id']) {
 
-
-	
 	echo "Name: ".$data[1] .' '. $data[2].' '. '<input type="submit" value="Edit"><br>';
 	echo "Username: ".$data[3].' '. '<input type="submit" value="Edit"><br>';
 	echo "Email: ".$data[6].' '. '<input type="submit" value="Edit"><br>';
