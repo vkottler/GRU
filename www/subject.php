e7ceaa5ea7834fb15769fa34dbdba6eab5f71421
vkottler <vkottler@wisc.edu>
2016-03-22 19:35:32 -0500

    Nothing crashed but for some reason can't see classes on subject page.

--- www/subject.php
+++ www/subject.php
@@ -10,15 +10,16 @@ if (isset($_GET['id'])) {
 		$data = $subjectData->fetch_row();
 		echo '<h2>'.$data[1].'</h2>';
 		echo $data[2].'<br><hr><br>';
-		$sql = 'SELECT * FROM `forumData`.`classes` WHERE `class_sub`='.$data[0];
+		$sql = 'SELECT * FROM `forumData`.`classes` WHERE `class_sub`='.$_GET['id'];
 		$result = $conn->query($sql);
 		if (!$result) echo 'We could not search for classes for this subject at this time.';
 		else if ($result->num_rows == 0) echo 'This subject doesn\'t have any classes yet!';
 		else {
+			$num_rows = $result->num_rows;
 			echo '<table>';
-			for ($i = 0; $i < $result->num_rows; $i++) {
-				$data = $result->fetch_row();
-				echo '<tr><td><a href="class.php?id='.$data[0].'">'.$data[1].'</a></td><td>'.$data[2].'</td></tr>';
+			for ($i = 0; $i < $num_rows; $i++) {
+				$data2 = $result->fetch_row();
+				echo '<tr><td><a href="class.php?id='.$data2[0].'">'.$data2[1].'</a></td><td>'.$data2[2].'</td></tr>';
 			}
 			echo '</table>';
 		}
