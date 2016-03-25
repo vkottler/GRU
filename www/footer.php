44c48dbb5193b8474ff233b29847c7ab17650cdc
vkottler <vkottler@wisc.edu>
2016-03-25 02:47:59 -0500

    Trying to add files again.

--- www/footer.php
+++ www/footer.php
@@ -2,5 +2,5 @@
 </div><!-- wrapper -->
 <div id="footer">Created by the Software Development Club</div>
 </body>
-<?php include 'scripts.php'; ?>
+<?php include '/var/git/GRUsite/www/scripts.php'; ?>
 </html>
