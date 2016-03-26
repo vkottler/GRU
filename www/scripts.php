bdf57f3bd3815ede1295fc04de5ab446f8624b3f
Vaughn Kottler <vkottler@wisc.edu>
2016-03-25 22:24:12 -0500

    Added cases for specific attributes.

--- www/scripts.php
+++ www/scripts.php
@@ -7,13 +7,16 @@ function revealForm(divName) { document.getElementById(divName).style.display = 
 function hideForm(divName) { document.getElementById(divName).style.display = "none"; }
 
 function changeHTMLback(id, form) { 
-	document.getElementById(id).innerHTML = '<input type="button" value= "Edit" onclick="changeAttribute(\'' + id + '\', \'' + form + '\')">';
+	if (id == "password") document.getElementById(id).innerHTML = '<input type="button" value="Change Password" onclick="changeAttribute(\''+id+'\', \''+form+'\')">';
+	document.getElementById(id).innerHTML = '<input type="button" value="Edit" onclick="changeAttribute(\'' + id + '\', \'' + form + '\')">';
 	shown--;
 	if (shown == 0) hideForm(form);
 }
 
 function changeAttribute(id, form) {
-	var newHTML = '<input type="text"> <input type="button" value="Hide" onClick="changeHTMLback(\'' + id + '\', \'' + form + '\')">';
+	var newHTML;
+	if (id == "name") newHTML = 'First: <input type="text"><br>Last: <input type="text"><br><input type="button" value="Hide" onclick="changeHTMLback(\''+id+'\', \''+form+'\')">';
+	newHTML = '<input type="text"> <input type="button" value="Hide" onclick="changeHTMLback(\'' + id + '\', \'' + form + '\')">';
 	document.getElementById(id).innerHTML = newHTML;
 	revealForm(form);
 	shown++;
