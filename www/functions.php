d53263675f6ae8445ceebd131cdaa53e08cd4595
Vaughn Kottler <vkottler@wisc.edu>
2016-04-10 18:15:16 -0500

    Used the wrong table name.

--- www/functions.php
+++ www/functions.php
@@ -78,7 +78,7 @@ function buildClassQuery($name, $description, $subject, $user) {
 }
 
 function buildPostQuery($title, $content, $user, $class) {
-    $sql = 'INSERT INTO `forumData`.`classes` (`post_by`, `post_class`, `post_content`, `post_title`, `post_date`)
+    $sql = 'INSERT INTO `forumData`.`posts` (`post_by`, `post_class`, `post_content`, `post_title`, `post_date`)
             VALUES ('.$user.', '.$class.', "'.$content.'", "'.$title.'", NOW())';
     return $sql;
 }
